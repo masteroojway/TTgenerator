85cba0b81abdf1456ede801ba8295fe202842898
Ujjwal jain <f20230602@goa.bits-pilani.ac.in>
2025-08-06 19:05:32 +0530

    added analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { inject } from '@vercel/analytics';
+
+if (import.meta.env.PROD) {
+  inject();
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
